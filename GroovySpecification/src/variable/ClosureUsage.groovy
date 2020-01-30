3e021ddf12cbf5bafb2b038baf932e411ad3b348
hjqxcode <hjqxcode@gmail.com>
2020-01-30 17:54:39 +0800

    20200130_closure this&owner&delegate and delegate strategy
    Signed-off-by: hjqxcode <hjqxcode@gmail.com>

--- GroovySpecification/src/variable/ClosureUsage.groovy
+++ GroovySpecification/src/variable/ClosureUsage.groovy
@@ -73,3 +73,56 @@ def collectResult = strWithNumber.collect {
     it.toUpperCase()
 }
 println collectResult // -->[W, E,  , H, A, V, E,  , 8,  , D, A, Y, S,  , 9,  , H, O, U, R, S,  , L, E, F, T]
+
+
+/*
+ * 闭包中三个重要变量this、owner、delegate
+ * this 代表闭包定义处的类
+ * owner 代表闭包定义处的类或对象
+ * delegate 代表任意对象, 默认以owner一致
+ */
+def outerClosure = {
+    def innerClosure = {
+        println "innerClosure this: " + this            // --> innerClosure this: variable.ClosureUsage@4d15107f
+        println "innerClosure owner: " + owner          // --> innerClosure owner: variable.ClosureUsage$_run_closure7@dc7df28
+        println "innerClosure delegate: " + delegate    // --> innerClosure delegate: variable.ClosureUsage$_run_closure7@dc7df28
+    }
+
+    println "innerClosure: " + innerClosure             // --> innerClosure: variable.ClosureUsage$_run_closure7$_closure11@69c81773
+    innerClosure.call()
+    println "outerClosure this: " + this                // --> outerClosure this: variable.ClosureUsage@4d15107f
+    println "outerClosure owner: " + owner              // --> outerClosure owner: variable.ClosureUsage@4d15107f
+    println "outerClosure delegate: " + delegate        // --> outerClosure delegate: variable.ClosureUsage@4d15107f
+}
+
+println "outerClosure: " + outerClosure                 // --> outerClosure: variable.ClosureUsage$_run_closure7@dc7df28
+outerClosure.call()
+
+/*
+ * 闭包委托策略
+ */
+class School {
+    String name
+    def outputName = {
+        "Name is ${name}"
+    }
+
+    @Override
+    String toString() {
+        outputName.call()
+    }
+}
+
+class ClassRoom {
+    String name
+}
+
+def school = new School(name : "Senior School")
+println school.toString()  // --> Name is Senior School
+
+def room = new School(name : "Senior Class")
+school.outputName.delegate = room
+school.outputName.resolveStrategy = groovy.lang.Closure.DELEGATE_FIRST
+println school.toString() // --> Name is Senior Class
+
+
